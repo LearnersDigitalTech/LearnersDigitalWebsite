729c10d7ffc82834593d321229cb5d38e274082c
kris22krishna <kris22krishna@gmail.com>
2025-12-08 20:05:23 +0530

    lol

--- src/app/math100/page.tsx
+++ src/app/math100/page.tsx
@@ -2,7 +2,7 @@ export default function Math100Page() {
     return (
         <main className="min-h-screen">
             {/* Content will be added here */}
-            <h1 className="text-2xl font-bold">Coming soon!</h1>
+            <h1 className="text-2xl font-bold">Coming soon!!</h1>
         </main>
     );
 }
